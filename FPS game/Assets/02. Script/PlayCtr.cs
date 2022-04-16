80ab9313b8bdbc9fa00a7a643ddb09b4c8175391
내 이름 < >
2022-04-17 02:05:52 +0900

    Update
    animation Update

--- FPS game/Assets/02. Script/PlayCtr.cs
+++ FPS game/Assets/02. Script/PlayCtr.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class PlayCtr : MonoBehaviour
 {
     
-    public float MoveSpeed = 3.0f;
+    public float MoveSpeed;
     public float RotateSpeed;
+
+
     
     Transform tf; //선언해주면 GetComponent 무조건 선언
+    Animation anim;
+
     void Start()
     {
         tf = GetComponent<Transform>();
+        anim = GetComponent<Animation>();
+
+        anim.Play("Idle");
     }
 
     
@@ -31,8 +38,36 @@ public class PlayCtr : MonoBehaviour
         //축*회전속도*프레임
         Vector3 rot = Vector3.up*r;
         tf.Rotate(rot*RotateSpeed*Time.deltaTime);
-
-        
     
+        AnimationClip(h,v);
+    }
+
+    void AnimationClip(float h, float v)
+    {
+        //키보드 입력에 따른 애니메이션 효과
+        if(v >= 0.1f)
+        {
+            anim.CrossFade("RunF",0.25f);
+        }
+
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade("RunB",0.25f);
+        }
+
+        else if (h >= 0.1f)
+        {
+            anim.CrossFade("RunR",0.25f);
+        }
+
+        else if (h <= -0.1f)
+        {
+            anim.CrossFade("RunL",0.25f);
+        }
+
+        else
+        {
+            anim.CrossFade("Idle",0.25f);
+        }
     }
 }
