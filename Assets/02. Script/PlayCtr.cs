2adf606de01f6c8ba5839d5b8399d1fc65b593ab
내 이름 <tjdckdwh@naver.com>
2022-06-11 04:17:00 +0900

    player Reload update
    player Reload update

--- Assets/02. Script/PlayCtr.cs
+++ Assets/02. Script/PlayCtr.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayCtr : MonoBehaviour
 {
     public float MoveSpeed = 8.0f;
     public float RotateSpeed = 30.0f;
+
+    
+    int bullet = 29;
+    public GameObject[] bulletsUI;
+    
+    public AudioClip Reload;
+    private new AudioSource audio;
 
 
     Transform tf; //선언해주면 GetComponent 무조건 선언
@@ -21,6 +29,7 @@ public class PlayCtr : MonoBehaviour
         tf = GetComponent<Transform>();
         anim = GetComponent<Animation>();
         fireCtrl = GetComponent<FireCtrl>();
+        audio = GetComponent<AudioSource>();
 
         anim.Play("Idle");
 
@@ -49,35 +58,20 @@ public class PlayCtr : MonoBehaviour
         //축*회전속도*프레임
         Vector3 rotX = Vector3.up*r;
         tf.Rotate(rotX*RotateSpeed*Time.deltaTime);
+        
     
-        
-        if(Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            if(i%2 == 1) //스프린트
-            {
-                Issprint = true;
-                MoveSpeed = 13.0f;
-                fireCtrl.enabled = false;
-            }
-            
-            else if(i%2 == 0) // 뛰기
-            {
-                Issprint = false;
-                MoveSpeed = 8.0f;
-                fireCtrl.enabled = true;
-            }
-                
-            i++;  //짝수 <-> 홀수  
-        }
-        
-        
+        //스프린트 행동
+        StartCoroutine(SprintAction());
+
+        //달리면서 총쏘기 행동
+        StartCoroutine(RunFireAction());
+
+        //키보드 입력에 따른 애니메이션 효과
         AnimationClip(h,v);
     }
 
-    
     void AnimationClip(float h, float v)
     {
-        //키보드 입력에 따른 애니메이션 효과
         if(v >= 0.1f)
         {
             anim.CrossFade((Issprint == true) ? "SprintF" : "RunF", 0.25f);
@@ -101,7 +95,13 @@ public class PlayCtr : MonoBehaviour
 
         else if(h == 0.0f && v==0.0f)
         {
-            StartCoroutine(IdleAction());
+            // sprint 후 멈추면 세팅 초기화
+            Issprint = false;
+            MoveSpeed = 8.0f;
+            fireCtrl.enabled = true;
+            i = 1;
+            
+            StartCoroutine(IdleReloadAction());
         }
 
         else
@@ -110,33 +110,79 @@ public class PlayCtr : MonoBehaviour
         }
     }
 
-    IEnumerator IdleAction()
+    //스프린트 행동
+    IEnumerator SprintAction()
     {
-        // sprint 후 멈추면 세팅 초기화
-        Issprint = false;
-        MoveSpeed = 8.0f;
-        fireCtrl.enabled = true;
-        i = 1;
-        
-        
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetKeyDown(KeyCode.LeftControl))
         {
-            anim.CrossFade("IdleFireSMG",0.25f);
-            yield return new WaitForSeconds(0.2f);
-            anim.CrossFade("Idle",0.25f);
+            if(i%2 == 1) //스프린트
+            {
+                Issprint = true;
+                MoveSpeed = 13.0f;
+                fireCtrl.enabled = false;
+            }
+            
+            else if(i%2 == 0) // 뛰기
+            {
+                Issprint = false;
+                MoveSpeed = 8.0f;
+                fireCtrl.enabled = true;
+            }
+                
+            i++;  //짝수 <-> 홀수 
+
+            yield return null; 
         }
-        
+    }
+
+    //달리면서 총쏠때 총알 UI
+    IEnumerator RunFireAction()
+    {
+        if(Input.GetMouseButtonDown(0) && Issprint == false) 
+        {
+            if(bullet < 0)
+            {
+                fireCtrl.enabled = false;
+                bullet = 0;
+                yield return null;
+            }
+            
+            bulletsUI[bullet].SetActive(false);
+            bullet--;
+
+            yield return null;
+        }
+    }
+    
+    
+    IEnumerator IdleReloadAction()
+    {    //멈춘 다음 재장전 행동
         if(Input.GetKeyDown(KeyCode.R))
         {
-            Debug.Log("Reload");
+            StartCoroutine(ReLoadBulletUI());
             
+            audio.PlayOneShot(Reload,1.0f);
             anim.CrossFade("IdleReloadSMG",0.25f);
+            
             yield return new WaitForSeconds(1.2f);
             anim.CrossFade("Idle",0.25f);
         }
     }
-           
 
+
+    //총알 UI 재장전    
+    IEnumerator ReLoadBulletUI()
+    {
+        for(bullet = 0; bullet < 30; bullet++)
+        {
+            bulletsUI[bullet].SetActive(true);
+            
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        bullet = 29;
+        
+    }
 
     
 }
